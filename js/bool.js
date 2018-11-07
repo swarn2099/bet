495758e842f8c42c687de158d7fc4281073e6456
Swarn Singh <swarn2099@gmail.com>
2018-11-06 22:38:53 -0600

    okay

--- js/bool.js
+++ js/bool.js
@@ -13,19 +13,7 @@ var inviteCodeArray, userInputCode, parent, newcode;
  */
 
 
- function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'tree.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);
- }
 
 
 $(document).ready(function() {
@@ -34,11 +22,9 @@ $(document).ready(function() {
   $("#nameCard").hide();
   $("#inviteOthers").hide();
   $("#tree").hide();
-  loadJSON(function(response) {
-   // Parse JSON string into object
-     var actual_JSON = JSON.parse(response);
-     console.log(actual_JSON);
-  });
+  $.getJSON( "tree.json", function( json ) {
+    console.log( "JSON Data received, name is " + json.name);
+});
 });
 
 /* Function to check invite code => checkInviteCode()
