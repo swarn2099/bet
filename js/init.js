5cd19419963e704d323e2a4d1adb0311393f8a42
Swarn Singh <swarn2099@gmail.com>
2018-11-15 04:37:04 -0600

    UI refresh

--- js/init.js
+++ js/init.js
@@ -1,6 +1,10 @@
 (function($) {
   $(function() {
     $('.modal').modal();
+    $('.carousel').carousel({
+      dist: -40,
+      padding: 200
+    });
     M.updateTextFields();
 
   }); // end of document ready
